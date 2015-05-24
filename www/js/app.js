b3f611ee4200b64a9de9ca99a98dfe076d7e80ce
Andrew Dinh <andrewdinh74@yahoo.com>
2015-05-23 19:45:01 -0700

    Add info for fb authentication

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,10 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var firebaseApp = angular.module('starter', ['ionic', 'firebase', 'ngCordovaOauth']);
+var fb = new Firebase('https://myfb.firebaseio.com')
 
-.run(function($ionicPlatform) {
+firebaseApp.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -16,4 +17,20 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+firebaseApp.controller('ExampleController', function($scope, $firebaseAuth, $cordovaOauth){
+  var auth = $firebaseAuth(fb);
+
+  $scope.login = function(){
+    $cordovaOauth.facebook(process.env.FACEBOOK_APP_ID, ['email']).then(function(result){
+      auth.$authWithOAuthToken('facebook', result.access_token).then(function(authData){
+        console.log(JSON.stringify(authData));
+      }, function(error){
+        console.error('ERROR: ' + error);
+      });
+    }, function(error){
+      console.error('ERROR: ' + error);
+    });
+  }
+});
